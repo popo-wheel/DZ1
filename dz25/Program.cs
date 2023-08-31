5d687556ef66c7faf8d4fc4ffc415984b89582a4
Petya <petya200606@gmail.com>
2023-09-01 01:23:45 +0800

    finish dz25

--- dz25/Program.cs
+++ dz25/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Алгоритм нужно реализовать самостоятельно, встроенную функцию использовать нельзя.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A：");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень данного числа ：");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+}
+
+Console.WriteLine($"Число {a} в степени {b} -> {result}");
