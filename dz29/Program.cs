509138f275f0569ce0340da6cc7ee827d8f73e6a
Petya <petya200606@gmail.com>
2023-09-01 01:26:02 +0800

    finish dz29

---
+++ dz29/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+void FillArrayRandom (int[] array)
+{
+
+ Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 34);
+
+}
+}
+
+void PrintArray1(int[] arr)
+ {
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]} ");
+        else Console.Write($"{arr[i]} -> ");
+    }
+ }
+
+void PrintArray2(int[] arr)
+ {
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+ }
+int[] arr = new int[8]; 
+FillArrayRandom(arr);
+PrintArray1(arr);
+ PrintArray2(arr);
